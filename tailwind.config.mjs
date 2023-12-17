7de99a351b70ec9c8c235c2456d5a677d5d847f9
leftra123 <eric.aguayo@inacapmail.cl>
2023-12-17 19:42:11 -0300

    Add new files and components

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			sans: ['Gothan SSm A', 'Gotham SSm B', 'sans-serif'],
+		},
+		extend: {},
+	},
+	plugins: [],
+}
